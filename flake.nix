4f0f5184c0f939c69c9f8ab23f32cbff333a618f
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-23 17:53:05 +0100

    flake: simplify bash init script

--- flake.nix
+++ flake.nix
@@ -104,9 +104,7 @@
                     # bash
                     ''
                       if [ "$(tty)" = "/dev/tty1" ]; then
-                        mkdir -p $HOME/.config/hypr
-                        cp ${hyprlandConf} $HOME/.config/hypr/hyprland.conf
-                        Hyprland
+                        Hyprland --config ${hyprlandConf}
                       fi
                     '';
                 };
