1ce94af119c1a7de2fd4c1477453df18918a9ab4
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-24 00:16:20 +0100

    examples: implement batch and refactor hyprctl

--- examples/hyprctl/main.go
+++ examples/hyprctl/main.go
@@ -6,12 +6,17 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/thiagokokada/hyprland-go"
 )
 
-var c *hyprland.RequestClient
+var (
+	c   *hyprland.RequestClient
+	out io.Writer = flag.CommandLine.Output()
+)
 
 // https://stackoverflow.com/a/28323276
 type arrayFlags []string
@@ -41,15 +46,19 @@ func mustMarshalIndent(v any) []byte {
 }
 
 func main() {
+	batchFS := flag.NewFlagSet("batch", flag.ExitOnError)
+	var batch arrayFlags
+	batchFS.Var(&batch, "c", "Command to batch, can be passed multiple times. "+
+		"Please quote commands with arguments (e.g.: 'dispatch exec kitty')")
+
 	dispatchFS := flag.NewFlagSet("dispatch", flag.ExitOnError)
 	var dispatch arrayFlags
-	dispatchFS.Var(&dispatch, "c", "Command to dispatch. Please quote commands with arguments (e.g.: 'exec kitty')")
+	dispatchFS.Var(&dispatch, "c", "Command to dispatch, can be passed multiple times. "+
+		"Please quote commands with arguments (e.g.: 'exec kitty')")
 
 	setcursorFS := flag.NewFlagSet("setcursor", flag.ExitOnError)
 	theme := setcursorFS.String("theme", "Adwaita", "Cursor theme")
 	size := setcursorFS.Int("size", 32, "Cursor size")
-
-	flag.Parse()
 
 	m := map[string]func(){
 		"activewindow": func() {
@@ -60,10 +69,25 @@ func main() {
 			v := must1(c.ActiveWorkspace())
 			fmt.Printf("%s\n", mustMarshalIndent(v))
 		},
+		"batch": func() {
+			batchFS.Parse(os.Args[2:])
+			if len(batch) == 0 {
+				fmt.Fprintf(out, "Error: at least one '-c' is required for batch.\n")
+				os.Exit(1)
+			} else {
+				// Batch commands are done in the following way:
+				// `[[BATCH]]command0 param0 param1; command1 param0 param1;`
+				r := hyprland.RawRequest(
+					fmt.Sprintf("[[BATCH]]%s", strings.Join(batch, ";")),
+				)
+				v := must1(c.RawRequest(r))
+				fmt.Printf("%s\n", v)
+			}
+		},
 		"dispatch": func() {
 			dispatchFS.Parse(os.Args[2:])
 			if len(dispatch) == 0 {
-				fmt.Println("-c is required for dispatch")
+				fmt.Fprintf(out, "Error: at least one '-c' is required for dispatch.\n")
 				os.Exit(1)
 			} else {
 				v := must1(c.Dispatch(dispatch...))
@@ -89,13 +113,18 @@ func main() {
 		},
 	}
 
-	if len(os.Args) < 2 {
-		fmt.Println("Usage:")
-		fmt.Printf("  %s [subcommand] <options>\n\n", os.Args[0])
-		fmt.Println("Available subcommands:")
+	flag.Usage = func() {
+		fmt.Fprintf(out, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(out, "  %s [subcommand] <options>\n\n", os.Args[0])
+		fmt.Fprintf(out, "Available subcommands:\n")
 		for k := range m {
-			fmt.Printf("  - %s\n", k)
+			fmt.Fprintf(out, "  - %s\n", k)
 		}
+	}
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		flag.Usage()
 		os.Exit(1)
 	}
 
@@ -104,7 +133,7 @@ func main() {
 		c = hyprland.MustClient()
 		run()
 	} else {
-		fmt.Printf("Unknown subcommand: %s\n", subcmd)
+		fmt.Fprintf(out, "Error: unknown subcommand: %s\n", subcmd)
 		os.Exit(1)
 	}
 }
