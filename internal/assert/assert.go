c30dc20b5acdf36224b353b0ae179a2f22434b27
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-25 00:59:11 +0100

    internal/assert: Fatalf -> Errorf

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -18,27 +18,27 @@ func Must(err error) {
 func Error(t *testing.T, err error) {
 	t.Helper()
 	if err == nil {
-		t.Fatalf("got: %v, want: !nil", err)
+		t.Errorf("got: %v, want: !nil", err)
 	}
 }
 
 func NoError(t *testing.T, err error) {
 	t.Helper()
 	if err != nil {
-		t.Fatalf("got: %v, want: nil", err)
+		t.Errorf("got: %v, want: nil", err)
 	}
 }
 
 func Equal[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
-		t.Fatalf("got: %v, want: %v", got, want)
+		t.Errorf("got: %v, want: %v", got, want)
 	}
 }
 
 func True(t *testing.T, got bool) {
 	t.Helper()
 	if got {
-		t.Fatalf("got: %v, want: false", got)
+		t.Errorf("got: %v, want: false", got)
 	}
 }
