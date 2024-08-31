5ef89fc673e066247aeb5249764fbeaff910fbfa
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-08-31 12:47:17 +0100

    event: import event code from labi-le/hyprland-ipc-client

--- event/event_test.go
+++ event/event_test.go
@@ -1,24 +1,119 @@
-package hyprland
+package event
 
 import (
-	"os"
 	"testing"
 )
 
 var ec *EventClient
 
-func init() {
-	if os.Getenv("HYPRLAND_INSTANCE_SIGNATURE") != "" {
-		ec = MustEventClient()
-	}
+var (
+	h = &FakeEventHandler{}
+	c = &FakeEventClient{}
+)
+
+type FakeEventClient struct {
+	EventClient
 }
 
-func TestReceive(t *testing.T) {
-	t.Skip("temporary disabled")
+type FakeEventHandler struct {
+	DummyEvHandler
+}
 
-	msg, err := ec.Receive()
+func (f *FakeEventClient) Receive() ([]ReceivedData, error) {
+	return []ReceivedData{
+		{
+			Type: EventWorkspace,
+			Data: "1",
+		},
+		{
+			Type: EventFocusedMonitor,
+			Data: "1,1",
+			// TODO I only have one monitor, so I didn't check this
+		},
+		{
+			Type: EventActiveWindow,
+			Data: "jetbrains-goland,hyprland-ipc-ipc – ipc.go",
+		},
+		{
+			Type: EventFullscreen,
+			Data: "1",
+		},
+		{
+			Type: EventMonitorRemoved,
+			Data: "1",
+			// TODO I only have one monitor, so I didn't check this
+		},
+		{
+			Type: EventMonitorAdded,
+			Data: "1",
+			// TODO I only have one monitor, so I didn't check this
+		},
+		{
+			Type: EventCreateWorkspace,
+			Data: "1",
+		},
+		{
+			Type: EventDestroyWorkspace,
+			Data: "1",
+		},
+
+		{
+			Type: EventMoveWorkspace,
+			Data: "1,1",
+			// TODO I only have one monitor, so I didn't check this
+		},
+		{
+			Type: EventActiveLayout,
+			Data: "AT Translated Set 2 keyboard,Russian",
+		},
+		{
+			Type: EventOpenWindow,
+			Data: "80e62df0,2,jetbrains-goland,win430",
+		},
+		{
+			Type: EventCloseWindow,
+			Data: "80e62df0",
+		},
+		{
+			Type: EventMoveWindow,
+			Data: "80e62df0,1",
+		},
+		{
+			Type: EventOpenLayer,
+			Data: "wofi",
+		},
+		{
+			Type: EventCloseLayer,
+			Data: "wofi",
+		},
+		{
+			Type: EventSubMap,
+			Data: "1",
+			// idk
+		},
+		{
+			Type: EventScreencast,
+			Data: "1,0",
+		},
+	}, nil
+}
+
+func TestSubscribe(t *testing.T) {
+	err := c.SubscribeWithoutLoop(h, GetAllEvents()...)
 	if err != nil {
 		t.Error(err)
 	}
-	t.Log(msg)
+}
+
+func (c *FakeEventClient) SubscribeWithoutLoop(ev EventHandler, events ...EventType) error {
+	msg, err := c.Receive()
+	if err != nil {
+		return err
+	}
+
+	for _, data := range msg {
+		processEvent(ev, data, events)
+	}
+
+	return nil
 }
