bbe89a77640210e746acfe52533d7e99b64e8a7b
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-09-04 09:50:45 +0100

    event/event.go: fix panic when the event becomes too small

--- event/event_test.go
+++ event/event_test.go
@@ -278,7 +278,7 @@ func BenchmarkReceive(b *testing.B) {
 // bottleneck
 // https://stackoverflow.com/a/31832326
 const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n"
 	letterIdxBits = 6                    // 6 bits to represent a letter index
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
 	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
